825d976fa7e2daf3fb93ad494e725f34b6eee7c0
Bilal Afzaal <bafzaal@uwo.ca>
2021-11-29 01:07:35 -0500

    Created Registration API

--- server/app.js
+++ server/app.js
@@ -10,12 +10,44 @@ const app = express();
 // Configure ENV file and require conn.js
 dotenv.config({path : './config.env'});
 require('./db/conn');
+const port = process.env.PORT;
+
+const Users = require('./models/userSchema');
+
+// Will be used to get data from the frontend
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.send("test"); 
 })
 
+// Register
+app.post('/register', async (req, res) => {
+    try {
+        // Get Data
+        const username = req.body.username;
+        const email = req.body.email;
+        const password = req.body.password;
+        
+        const createUser = new Users({
+            username : username,
+            email : email,
+            password : password
+        });
+
+        // save will create or insert the user after hashing, then it will save to db
+        const created = await createUser.save();
+        console.log(created);
+        res.status(200).send("Registration Complete");
+        
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
+
 // Run the server
-app.listen(3001, () => {
+app.listen(port, () => {
     console.log("Server Started")
 })
