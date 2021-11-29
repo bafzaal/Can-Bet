5304b331bc576b7184478b8518244a7a3985800b
Bilal Afzaal <bafzaal@uwo.ca>
2021-11-28 23:51:41 -0500

    Initialized the server

---
+++ server/app.js
@@ -0,0 +1,18 @@
+// Import all dependencies 
+const dotenv = require('dotenv');
+const express = require('express');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser'); // Might not use this, depending on time
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("test"); 
+})
+
+// Run the server
+app.listen(3001, () => {
+    console.log("Server Started")
+})
