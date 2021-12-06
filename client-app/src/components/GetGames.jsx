97a73e341059e0cb3303acb6be406b342d2c52b8
Bilal Afzaal <bafzaal@uwo.ca>
2021-12-06 02:56:58 -0500

    Added loading spinner to allow application time to fetch data from API

--- client-app/src/components/GetGames.jsx
+++ client-app/src/components/GetGames.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
 import PropTypes from 'prop-types';
 import NFL from '../images/NFL.png'
 import UNIBET from '../images/UNIBET.jpeg'
@@ -15,25 +16,32 @@ GetGames.defaultProps = {
     games: [],
 };
 
-function GetAmericanOdds(odds)
-{
+function GetAmericanOdds(odds) {
     const americanOdds = []
-    if(odds[0] >= 2)
-        americanOdds.push('+' + Math.round((odds[0] - 1)*100))
+    if (odds[0] >= 2)
+        americanOdds.push('+' + Math.round((odds[0] - 1) * 100))
     else if (odds[0] < 2)
-        americanOdds.push(Math.round(-100/(odds[0] - 1)))
+        americanOdds.push(Math.round(-100 / (odds[0] - 1)))
 
-    if(odds[1] >= 2)
-        americanOdds.push('+' + Math.round((odds[1] - 1)*100))
+    if (odds[1] >= 2)
+        americanOdds.push('+' + Math.round((odds[1] - 1) * 100))
     else if (odds[1] < 2)
-        americanOdds.push(Math.round(-100/(odds[1] - 1)))
+        americanOdds.push(Math.round(-100 / (odds[1] - 1)))
 
     return americanOdds
 }
 
 function GetGames(props) {
     const { games } = props
-    console.log(games)
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 1000)
+    }, [])
 
     const mapGames = [] // [{game name, unitbet, ...}] game name, map to values
     games.forEach(function (item) {
@@ -82,40 +90,44 @@ function GetGames(props) {
     console.log("hi", mapGames)
     return (
         <div>
-            {/* <ul className='game-list'>
-                {games.map(game => (
-                    <li key={game.id}>{game.teams}</li>
-                ))}
-            </ul> */}
-            <h1 className="text-center heading">NFL MONEYLINE ODDS</h1>
-            <ReactBootStrap.Table striped bordered hover responsive className="OddsTable">
-                <thead>
-                    <tr>
-                        <td className="text-center NFLLogo"><img width="72" height="100" src={NFL} /></td>
-                        <td width="200" height="80"><img width="160" height="100" src={UNIBET} /></td>
-                        <td width="200" height="80"><img width="160" height="100" src={BarstoolSportsbook} /></td>
-                        <td width="200" height="80"><img width="160" height="100" src={DraftKings} /></td>
-                        <td width="200" height="80"><img width="160" height="100" src={FanDuel} /></td>
-                        <td width="200" height="80"><img width="160" height="100" src={FoxBet} /></td>
-                    </tr>
-                </thead>
-                <tbody>
+            {
+                loading ?
+                    <div className="loading">
+                        <ClipLoader color={"#D74C36"} loading={loading} size={100} />
+                    </div>
+                    :
+                    <>
+                        <h1 className="text-center heading">NFL MONEYLINE ODDS</h1>
+                        <ReactBootStrap.Table striped bordered hover responsive className="OddsTable">
+                            <thead>
+                                <tr>
+                                    <td className="text-center NFLLogo"><img width="72" height="100" src={NFL} /></td>
+                                    <td width="200" height="80"><img width="160" height="100" src={UNIBET} /></td>
+                                    <td width="200" height="80"><img width="160" height="100" src={BarstoolSportsbook} /></td>
+                                    <td width="200" height="80"><img width="160" height="100" src={DraftKings} /></td>
+                                    <td width="200" height="80"><img width="160" height="100" src={FanDuel} /></td>
+                                    <td width="200" height="80"><img width="160" height="100" src={FoxBet} /></td>
+                                </tr>
+                            </thead>
+                            <tbody>
 
-                    {
-                        mapGames.map(game => {
-                            return <tr key={game.id}>
-                                <td className="text-center">{game.gameNames[0]} <br />{game.gameNames[1]} </td>
-                                <td className="text-center">{game.oddsMap.unibet[0]} / {game.oddsMap.unibet[2]} <br />{game.oddsMap.unibet[1]} / {game.oddsMap.unibet[3]}</td>
-                                <td className="text-center">{game.oddsMap.barstool[0]} / {game.oddsMap.barstool[2]} <br />{game.oddsMap.barstool[1]} / {game.oddsMap.barstool[3]}</td>
-                                <td className="text-center">{game.oddsMap.draftkings[0]} / {game.oddsMap.draftkings[2]} <br />{game.oddsMap.draftkings[1]} / {game.oddsMap.draftkings[3]}</td>
-                                <td className="text-center">{game.oddsMap.fanduel[0]} / {game.oddsMap.fanduel[2]} <br />{game.oddsMap.fanduel[1]} / {game.oddsMap.fanduel[3]}</td>
-                                <td className="text-center">{game.oddsMap.foxbet[0]} / {game.oddsMap.foxbet[2]} <br />{game.oddsMap.foxbet[1]} / {game.oddsMap.foxbet[3]}</td>
-                            </tr>
-                        })
-                    }
+                                {
+                                    mapGames.map(game => {
+                                        return <tr key={game.id}>
+                                            <td className="text-center">{game.gameNames[0]} <br />{game.gameNames[1]} </td>
+                                            <td className="text-center">{game.oddsMap.unibet[0]} / {game.oddsMap.unibet[2]} <br />{game.oddsMap.unibet[1]} / {game.oddsMap.unibet[3]}</td>
+                                            <td className="text-center">{game.oddsMap.barstool[0]} / {game.oddsMap.barstool[2]} <br />{game.oddsMap.barstool[1]} / {game.oddsMap.barstool[3]}</td>
+                                            <td className="text-center">{game.oddsMap.draftkings[0]} / {game.oddsMap.draftkings[2]} <br />{game.oddsMap.draftkings[1]} / {game.oddsMap.draftkings[3]}</td>
+                                            <td className="text-center">{game.oddsMap.fanduel[0]} / {game.oddsMap.fanduel[2]} <br />{game.oddsMap.fanduel[1]} / {game.oddsMap.fanduel[3]}</td>
+                                            <td className="text-center">{game.oddsMap.foxbet[0]} / {game.oddsMap.foxbet[2]} <br />{game.oddsMap.foxbet[1]} / {game.oddsMap.foxbet[3]}</td>
+                                        </tr>
+                                    })
+                                }
 
-                </tbody>
-            </ReactBootStrap.Table>
+                            </tbody>
+                        </ReactBootStrap.Table>
+                    </>
+            }
         </div>
 
     )
