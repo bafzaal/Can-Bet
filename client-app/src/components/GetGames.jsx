6b1e06e37766fe8e049f25de8692e24135a0d9eb
Bilal Afzaal <bafzaal@uwo.ca>
2021-12-06 02:33:52 -0500

    Added logic for American betting odds

--- client-app/src/components/GetGames.jsx
+++ client-app/src/components/GetGames.jsx
@@ -15,6 +15,22 @@ GetGames.defaultProps = {
     games: [],
 };
 
+function GetAmericanOdds(odds)
+{
+    const americanOdds = []
+    if(odds[0] >= 2)
+        americanOdds.push('+' + Math.round((odds[0] - 1)*100))
+    else if (odds[0] < 2)
+        americanOdds.push(Math.round(-100/(odds[0] - 1)))
+
+    if(odds[1] >= 2)
+        americanOdds.push('+' + Math.round((odds[1] - 1)*100))
+    else if (odds[1] < 2)
+        americanOdds.push(Math.round(-100/(odds[1] - 1)))
+
+    return americanOdds
+}
+
 function GetGames(props) {
     const { games } = props
     console.log(games)
@@ -30,27 +46,39 @@ function GetGames(props) {
             if (site.site_key == "unibet") {
                 unibet.push(site.odds.h2h[0])
                 unibet.push(site.odds.h2h[1])
+                unibet.push(GetAmericanOdds(site.odds.h2h)[0])
+                unibet.push(GetAmericanOdds(site.odds.h2h)[1])
             }
             if (site.site_key == "barstool") {
                 barstool.push(site.odds.h2h[0])
                 barstool.push(site.odds.h2h[1])
+                barstool.push(GetAmericanOdds(site.odds.h2h)[0])
+                barstool.push(GetAmericanOdds(site.odds.h2h)[1])
             }
             if (site.site_key == "draftkings") {
                 draftkings.push(site.odds.h2h[0])
                 draftkings.push(site.odds.h2h[1])
+                draftkings.push(GetAmericanOdds(site.odds.h2h)[0])
+                draftkings.push(GetAmericanOdds(site.odds.h2h)[1])
             }
             if (site.site_key == "fanduel") {
                 fanduel.push(site.odds.h2h[0])
                 fanduel.push(site.odds.h2h[1])
+                fanduel.push(GetAmericanOdds(site.odds.h2h)[0])
+                fanduel.push(GetAmericanOdds(site.odds.h2h)[1])
             }
             if (site.site_key == "foxbet") {
                 foxbet.push(site.odds.h2h[0])
                 foxbet.push(site.odds.h2h[1])
+                foxbet.push(GetAmericanOdds(site.odds.h2h)[0])
+                foxbet.push(GetAmericanOdds(site.odds.h2h)[1])
             }
 
         });
         mapGames.push({ gameNames: item.teams, oddsMap: { unibet: unibet, barstool: barstool, draftkings: draftkings, fanduel: fanduel, foxbet: foxbet } })
     });
+
+
     console.log("hi", mapGames)
     return (
         <div>
@@ -77,11 +105,11 @@ function GetGames(props) {
                         mapGames.map(game => {
                             return <tr key={game.id}>
                                 <td className="text-center">{game.gameNames[0]} <br />{game.gameNames[1]} </td>
-                                <td className="text-center">{game.oddsMap.unibet[0]} <br />{game.oddsMap.unibet[1]}</td>
-                                <td className="text-center">{game.oddsMap.barstool[0]} <br />{game.oddsMap.barstool[1]}</td>
-                                <td className="text-center">{game.oddsMap.draftkings[0]} <br />{game.oddsMap.draftkings[1]}</td>
-                                <td className="text-center">{game.oddsMap.fanduel[0]} <br />{game.oddsMap.fanduel[1]}</td>
-                                <td className="text-center">{game.oddsMap.foxbet[0]} <br />{game.oddsMap.foxbet[1]}</td>
+                                <td className="text-center">{game.oddsMap.unibet[0]} / {game.oddsMap.unibet[2]} <br />{game.oddsMap.unibet[1]} / {game.oddsMap.unibet[3]}</td>
+                                <td className="text-center">{game.oddsMap.barstool[0]} / {game.oddsMap.barstool[2]} <br />{game.oddsMap.barstool[1]} / {game.oddsMap.barstool[3]}</td>
+                                <td className="text-center">{game.oddsMap.draftkings[0]} / {game.oddsMap.draftkings[2]} <br />{game.oddsMap.draftkings[1]} / {game.oddsMap.draftkings[3]}</td>
+                                <td className="text-center">{game.oddsMap.fanduel[0]} / {game.oddsMap.fanduel[2]} <br />{game.oddsMap.fanduel[1]} / {game.oddsMap.fanduel[3]}</td>
+                                <td className="text-center">{game.oddsMap.foxbet[0]} / {game.oddsMap.foxbet[2]} <br />{game.oddsMap.foxbet[1]} / {game.oddsMap.foxbet[3]}</td>
                             </tr>
                         })
                     }
