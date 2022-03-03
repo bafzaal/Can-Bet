ee343e50f5ea5cc3a4e80dc7de540cd718cca38b
oabdalle <oabdalle@uwo.ca>
2022-03-03 01:35:57 -0500

    Betting tips blurb

--- client-app/src/components/BettingTips.jsx
+++ client-app/src/components/BettingTips.jsx
@@ -8,6 +8,11 @@ const BettingTips = (props) => {
                 <Row>
                       <h1 className="text-center headingLine">Betting Tips</h1>
                 </Row>
+                <Row>
+                <p>It can be frusturating to lose sometimes, especially with money on the line. Please take some time and visit some resources like <a href="https://www.thesportsgeek.com/blog/11-sports-betting-strategies-for-beginners/">The Sports Geek</a> so you can 
+                get a better grasp of what betting should be like. There are other external resources you can use to improve and we strongly suggest that you know
+                what you are doing before beginning to bet. Remember, we are on your side!</p>
+            </Row>
             </Container>
         </>
     )
